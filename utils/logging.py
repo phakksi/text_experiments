5f43adffa3d4b9cdc0a6c83579a132ae233204c2
seles quarian <selesquarian@pm.me>
2023-04-01 14:19:39 -0400

    new logging logic and util folder

--- utils/logging.py
+++ utils/logging.py
@@ -7,7 +7,7 @@ logging.basicConfig(filename=f'logs/{timestr}.log',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',  # noqa: E501
                     datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
